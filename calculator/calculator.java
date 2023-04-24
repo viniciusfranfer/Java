8723cb58d962b68cb978d1fd293302a601a81b41
Vinicius França <114887277+viniciusfranfer@users.noreply.github.com>
2023-04-24 17:32:10 -0300

    Update calculator.java

---
+++ calculator/calculator.java
@@ -0,0 +1,31 @@
+public class Calculator {
+
+  public Calculator(){
+
+  }
+
+  public int add(int a, int b){
+    return a + b;
+  }
+  public int subtract(int a, int b){
+    return a - b;
+  }
+  public int multiply(int a, int b){
+    return a * b;
+  }
+  public int divide(int a, int b){
+    return a / b;
+  }
+  public int modulo(int a, int b){
+    return a % b;
+  }
+
+  public static void main(String[] args){
+    Calculator myCalculator = new Calculator();
+    System.out.println(myCalculator.add(5, 7));
+    System.out.println(myCalculator.subtract(45, 11));
+    System.out.println(myCalculator.multiply(10, 3));
+    System.out.println(myCalculator.divide(40, 2));
+  }
+
+}
